5f4d9acef797c055e789b4aec2796093ce0b991e
Shalini20052204 <perumallashalini631@gmail.com>
2025-02-28 11:50:50 +0530

    Update college.py

--- college.py
+++ college.py
@@ -24,7 +24,7 @@ df['Answer'] = df['Answer'].str.lower()
 vectorizer = TfidfVectorizer()
 question_vectors = vectorizer.fit_transform(df['Question'])
 
-API_KEY = "AIzaSyDm2kDgJI6OhULuBF3p5Cc1UHBIK7keWj4" 
+API_KEY = "AIzaSyCbzLaP0CnIKKd4rVsImRUHThLOA-0HxBc" 
 
 genai.configure(api_key=API_KEY)
 model = genai.GenerativeModel('gemini-1.5-flash')
